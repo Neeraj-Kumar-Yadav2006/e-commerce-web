89177edd24c35057cac4b77bc57c845a7f4bbbee
Nk <nk@example.com>
2026-03-10 02:00:07 +0530

    Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 //const express = require('express');
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port=process.env.PORT || 5000;
+connectDB();
 const app=express();
 app.get('/',(req,res)=>{
     res.send('API is running')
