99b81f5cbd9cf08055864b5c9356084eb7a74fd0
Nk <nk@example.com>
2026-03-11 12:44:06 +0530

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 //const express = require('express');
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port=process.env.PORT || 5000;
 connectDB();
 const app=express();
 app.get('/',(req,res)=>{
     res.send('API is running')
 });
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Cookie Parser middleware
+app.use(cookieParser());
 // app.get('/api/products',(req,res)=>{
 //     res.json(products);
 // });
@@ -19,6 +26,7 @@ app.get('/',(req,res)=>{
 //     res.json(product);
 // });
 app.use('/api/products',productRoutes);
+app.use('/api/users',userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port,()=>console.log(`Server running on port ${port}`));
